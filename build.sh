78ded05954ecda353368c6a9d80cc2ef0e65f432
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-09-01 14:22:22 +0100

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+# Reduce npm logging
+npm config set loglevel warn
+npm install -g marked
+
+pushd Lambda
+
+
+for directory in $(ls -d */ | sed 's#/##'); do
+  echo "Building: $directory"
+  pushd $directory
+
+  rm -f *.zip || true
+
+  for f in $(ls -d */ | sed 's#/##'); do
+    echo "Building $f"
+    pushd $f
+    npm install
+    popd
+    echo "Zipping $f"
+    zip -q -9 -r ../../PackagedLambdas/$f.zip $f/*
+  done
+
+  popd
+done
+
+popd
